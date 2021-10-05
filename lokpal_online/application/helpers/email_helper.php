7c0cd878eb23550f4907321165d9404a6bce25e2
rohitcode3004 <bishtrohit3004@gmail.com>
2021-10-05 12:10:32 +0530

    email_helper updated for both local host and server

--- lokpal_online/application/helpers/email_helper.php
+++ lokpal_online/application/helpers/email_helper.php
@@ -8,7 +8,7 @@ function sendMail($email,$subject,$html)
   	'protocol' => 'smtp',
   	'smtp_host' => 'ssl://smtp.googlemail.com',
   	'smtp_port' => 465,
-  	'smtp_user' => '007rohit.bisht@gmail.com', // change it to yours
+  	'smtp_user' => 'cietrohit@gmail.com', // change it to yours
   	'smtp_pass' => '01382.ktd', // change it to yours
   	'mailtype' => 'html',
   	'charset' => 'iso-8859-1',
@@ -31,6 +31,51 @@ function sendMail($email,$subject,$html)
     	{
      	show_error($CI->email->print_debugger());
     }
+ }
+
+ function sendMail_2($email,$subject,$html)
+ {
+      $CI =& get_instance();
+      $CI->load->library('PHPMailer_Lib');
+
+      $objMail = $CI->phpmailer_lib->load();
+        // SMTP configuration
+        $objMail->isSMTP();
+        $objMail->Host     = 'ssl://smtp.googlemail.com';
+        $objMail->SMTPAuth = true;
+        $objMail->Username = '007rohit.bisht@gmail.com';
+        $objMail->Password = '01382.ktd';
+        $objMail->SMTPSecure = 'ssl';
+        $objMail->Port     = 465;
+        
+        $objMail->setFrom('info@example.com', 'CodexWorld');
+        $objMail->addReplyTo('info@example.com', 'CodexWorld');
+        
+        // Add a recipient
+        $objMail->addAddress($email);
+        
+        // Add cc or bcc 
+        $objMail->addCC('cc@example.com');
+        $objMail->addBCC('bcc@example.com');
+        
+        // Email subject
+        $objMail->Subject = $subject;
+        
+        // Set email format to HTML
+        $objMail->isHTML(true);
+        
+        // Email body content
+        //$mailContent = "<h1>Send HTML Email using SMTP in CodeIgniter</h1>
+            //<p>This is a test email sending using SMTP mail server with PHPMailer.</p>";
+        $objMail->Body = $html;
+        
+        // Send email
+        if(!$objMail->send()){
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $objMail->ErrorInfo;
+        }else{
+            return 1;
+        }
  }
  
 }
